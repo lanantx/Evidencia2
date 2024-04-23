6ad737fe0be275001cdf9081e863030ebf1ac5ab
Francisco Huerta Kretsch <francisco.huertak@uanl.edu.mx>
2024-04-23 16:11:43 -0600

    Se añaden metodos al buitre

--- FranciscoHuertaK/Buitre.py
+++ FranciscoHuertaK/Buitre.py
@@ -5,3 +5,19 @@ class Buitre:
         self.__sexo = sexo
         self.__altura = altura
         self.__peso = peso
+
+    def alimentacion(self):
+        print("Los buitres se alimentan de carroña.")
+
+    def caracteristicas(self):
+        print("Una de las caracteristicas de los buitres es que no tienen plumas en la cabeza.")
+
+    def conservacion(self):
+        print("Alrededor de la mitad de especies estan en peligro de extincion.")
+
+    def distribucion(self):
+        print("Los buitres se hallan en Europa, Africa, Asia y America")
+
+    def curiosidad(self):
+        print("Los buitres del viejo y nuevo mundo no estan tan emparentados y su parentezco es resultado de evolucion convergente.")
+
