22f9ef4fa61aa0b60f928aa47096a865009e2145
Francisco Huerta Kretsch <francisco.huertak@uanl.edu.mx>
2024-04-23 10:27:36 -0600

    Se crea la carpeta, el archivo de buitre y su init

---
+++ FranciscoHuertaK/Buitre.py
@@ -0,0 +1,7 @@
+class Buitre:
+    def __init__(self,nombre,edad,sexo,altura,peso):
+        self.__nombre = nombre
+        self.__edad = edad
+        self.__sexo = sexo
+        self.__altura = altura
+        self.__peso = peso
