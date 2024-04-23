b68a003f1c18ea1aa903de6ecf5ccb9de3679b10
saulguglielmi <saulguglielmi@gmail.com>
2024-04-22 21:59:39 -0600

    solo lo hice chikito

--- saulguglielmi/Zorro.py
+++ saulguglielmi/Zorro.py
@@ -1,8 +1,6 @@
 from Animal import Animal
-
 class Zorro(Animal):
     def __init__(self, nombre):
         super().__init__(nombre, "Zorro")
-
     def hacer_sonido(self):
         return "WHAT THE FOX SAY"
