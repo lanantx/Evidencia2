5b937a698cd44d80414cbc5d232dde540f972804
saulguglielmi <saulguglielmi@gmail.com>
2024-04-22 21:37:07 -0600

    se ha añadido un bucle

--- saulguglielmi/Code.py
+++ saulguglielmi/Code.py
@@ -1,11 +1,17 @@
 from Koi import koi
 from Panda_Rojo import panda_rojo
 from Zorro import zorro
-print("Pez Koi Inserte 1\nPanda Rojo Inserte 2, Zorro Inserte 3")
-opcion=input("Que opcion buscas")
-if opcion =='1':
-    koi()
-elif opcion =='2':
-    panda_rojo()
-elif opcion =='3':
-    zorro()
+
+while True:
+    print("Pez Koi: Inserte 1\nPanda Rojo: Inserte 2\nZorro: Inserte 3")
+    opcion = input("¿Qué opción buscas?\n(Presiona enter para salir): ")
+
+    if opcion == '1':
+        koi()
+    elif opcion == '2':
+        panda_rojo()
+    elif opcion == '3':
+        zorro()
+    else:
+        print("Saliendo del programa...")
+        break
