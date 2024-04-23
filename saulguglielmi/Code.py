b8a62a94cab376b5fcbc2cee27d7c402ff502275
saulguglielmi <saulguglielmi@gmail.com>
2024-04-22 21:13:59 -0600

    Se añadio el codigo

---
+++ saulguglielmi/Code.py
@@ -0,0 +1,11 @@
+from saulguglielmi import Koi
+from saulguglielmi import Panda_Rojo
+from saulguglielmi import Zorro
+print("Pez Koi Inserte 1\nPanda Rojo Inserte 2, Zorro Inserte 3")
+opcion=input("Que opcion buscas")
+if opcion =='1':
+    Koi()
+if opcion =='2':
+    Panda_Rojo()
+if opcion =='3':
+    Zorro()
