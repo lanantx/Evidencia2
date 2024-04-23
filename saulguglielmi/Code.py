415aebb5993b4cbdd3c2227e1bd37ff72c85d10d
saulguglielmi <saulguglielmi@gmail.com>
2024-04-22 21:58:28 -0600

    subi todo de chingadazo ahorita cambio todo pa que se vean los ultimos cambios

--- saulguglielmi/Code.py
+++ saulguglielmi/Code.py
@@ -1,17 +1,54 @@
-from Koi import koi
-from Panda_Rojo import panda_rojo
-from Zorro import zorro
+from Koi import Koi
+from Panda_Rojo import Panda_Rojo
+from Zorro import Zorro
 
-while True:
-    print("Pez Koi: Inserte 1\nPanda Rojo: Inserte 2\nZorro: Inserte 3")
-    opcion = input("¿Qué opción buscas?\n(Presiona enter para salir): ")
+def agregar_animal(mini_zoo):
+    print("Seleccione el tipo de animal a agregar:")
+    print("1. Koi")
+    print("2. Panda Rojo")
+    print("3. Zorro")
+    opcion = int(input())
 
-    if opcion == '1':
-        koi()
-    elif opcion == '2':
-        panda_rojo()
-    elif opcion == '3':
-        zorro()
+    nombre = input("Ingrese el nombre del animal: ")
+
+    if opcion == 1:
+        animal = Koi(nombre)
+    elif opcion == 2:
+        animal = Panda_Rojo(nombre)
+    elif opcion == 3:
+        animal = Zorro(nombre)
     else:
-        print("Saliendo del programa...")
-        break
+        print("Opción no válida")
+        return
+
+    mini_zoo.append(animal)
+    print("Animal agregado con éxito!")
+
+def ver_animales(mini_zoo):
+    print("Animales en el mini-zoo:")
+    for animal in mini_zoo:
+        print(f"Nombre: {animal.get_nombre()}, Especie: {animal.get_especie()}, Sonido: {animal.hacer_sonido()}")
+
+def menu():
+    mini_zoo = []
+
+    while True:
+        print("Opciones del programa:")
+        print("1. Agregar animal")
+        print("2. Ver todos los animales del mini-zoo")
+        print("3. Salir")
+
+        opcion = int(input("Seleccione una opción: "))
+
+        if opcion == 1:
+            agregar_animal(mini_zoo)
+        elif opcion == 2:
+            ver_animales(mini_zoo)
+        elif opcion == 3:
+            break
+        else:
+            print("Opción no válida")
+
+if __name__ == "__main__":
+    menu()
+
