7c5d16ea25d36905ebf2a8be1185de3bb57e7416
Francisco Huerta Kretsch <francisco.huertak@uanl.edu.mx>
2024-04-24 16:05:39 -0600

    Se añaden metodos al pulpo

--- FranciscoHuertaK/Pulpo.py
+++ FranciscoHuertaK/Pulpo.py
@@ -5,3 +5,18 @@ class Pulpo:
         self.__sexo = sexo
         self.__largo = largo
         self.__peso = peso
+
+    def alimentacion(self):
+        print("Los pulpos comen carne.")
+
+    def caracteristicas(self):
+        print("Los pulpos tienen 8 extremidades.")
+
+    def conservacion(self):
+        print("Se cree no estan en peligro.")
+
+    def distribucion(self):
+        print("Los pulpos se hallan en todos los oceanos.")
+
+    def orden(self):
+        print("Pertenecen al orden Octopoda")
