dcdf3511e05bb59ced5ce95fb831ef446058e2d3
Diego Emilio Ramirez Garcia <diego.emilio.raga@gmail.com>
2024-04-24 17:09:21 -0600

    init Camaleon

---
+++ DiegooRamirez/Camaleon.py
@@ -0,0 +1,7 @@
+class Camaleon:
+    def __init__(self, Nombre, Edad, Peso, Tamaño, Color):
+        self.__Nombre=Nombre
+        self.__Edad=Edad
+        self.__Peso=Peso
+        self.__Tamaño=Tamaño
+        self.__Color=Color
