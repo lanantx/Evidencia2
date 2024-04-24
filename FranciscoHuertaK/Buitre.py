0798ff91508e6b3290eb5ff4405ae28a5438601a
Francisco Huerta Kretsch <francisco.huertak@uanl.edu.mx>
2024-04-23 23:43:12 -0600

    Se añaden los getters y setters del buitre

--- FranciscoHuertaK/Buitre.py
+++ FranciscoHuertaK/Buitre.py
@@ -21,3 +21,44 @@ class Buitre:
     def curiosidad(self):
         print("Los buitres del viejo y nuevo mundo no estan tan emparentados y su parentezco es resultado de evolucion convergente.")
 
+    @property
+    def nombre(self):
+        return self.__nombre
+    
+    @nombre.setter
+    def nombre(self,value):
+        self.__nombre = value
+
+    @property
+    def edad(self):
+        return self.__edad
+    
+    @edad.setter
+    def edad(self,value):
+        self.__edad = value
+
+    @property
+    def sexo(self):
+        return self.__sexo
+    
+    @sexo.setter
+    def sexo(self,value):
+        self.__sexo = value
+
+    @property
+    def altura(self):
+        return self.__altura
+    
+    @altura.setter
+    def altura(self,value):
+        self.__altura = value
+
+    @property
+    def peso(self):
+        return self.__peso
+    
+    @peso.setter
+    def peso(self,value):
+        self.__peso = value
+
+        
