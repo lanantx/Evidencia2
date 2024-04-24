b6f09c83ea12e34abf1f5f21974ed8dd22124175
Brissa Marisol Flores Tamez <brissa.florest@uanl.edu.mx>
2024-04-24 17:15:52 -0600

    Atributos de animales

--- brissaflorest/Koala.py
+++ brissaflorest/Koala.py
@@ -1,2 +1,38 @@
 class koala:
-    pass
+    def __init__(self,nombre,edad,tamaño,peso):
+        self.__nombre = nombre
+        self.__edad = edad
+        self.__tamaño = tamaño
+        self.__peso = peso
+
+    @property
+    def nombre(self):
+        return self.__nombre
+    
+    @nombre.setter
+    def nombre(self,valor):
+        self.__nombre = valor   
+    
+    @property
+    def edad(self):
+        return self.__edad
+    
+    @edad.setter
+    def edad(self,valor):
+        self.__edad = valor   
+
+    @property
+    def tamaño(self):
+        return self.__tamaño
+    
+    @tamaño.setter
+    def tamaño(self,valor):
+        self.__tamaño = valor   
+
+    @property
+    def peso(self):
+        return self.__peso
+    
+    @peso.setter
+    def peso(self,valor):
+        self.__peso = valor
