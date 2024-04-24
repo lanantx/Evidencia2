71f9240581a295ded39a48bebf296e4174e2b924
MrRobis <RobertoEstradaSandoval@hotmail.com>
2024-04-24 15:15:40 -0600

    Setters y getters agregados

--- Robs115/Cocodrilo.py
+++ Robs115/Cocodrilo.py
@@ -6,7 +6,40 @@ class Cocodrilo:
         self.__Tamaño = Tamaño
         self.__ComidaFav = ComidaFav
         
-   
+    @property
+    def ComidaFav (self):
+        return self.__ComidaFav
+    @ComidaFav.getter
+    def ComidaFav (self,Value):
+        Value = self.__ComidaFav
+    
+    @property
+    def Nombre (self):
+        return self.__Nombre
+    @Nombre.getter
+    def Nombre (self,Value):
+        Value = self.__Nombre
+    
+    @property
+    def Edad (self):
+        return self.__Edad
+    @Edad.getter
+    def ComidaFav (self,Value):
+        Value = self.__Edad
+    
+    @property
+    def Peso (self):
+        return self.__Peso
+    @Peso.getter
+    def Peso (self,Value):
+        Value = self.__Peso
+    
+    @property
+    def Tamaño (self):
+        return self.__Tamaño
+    @Tamaño.getter
+    def ComidaFav (self,Value):
+        Value = self.__Tamaño
 
     def Comer(self):
         print(f"*{self.__Nombre} Come un pescado*")
