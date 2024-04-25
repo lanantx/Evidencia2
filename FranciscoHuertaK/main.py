a807e28680c092b4f9c08906220417d3bf8424f3
Francisco Huerta Kretsch <francisco.huertak@uanl.edu.mx>
2024-04-25 10:23:57 -0600

    Se hace el menu

---
+++ FranciscoHuertaK/main.py
@@ -0,0 +1,10 @@
+
+
+while True:
+    opcion = int(input("Elige una opcion: \n 1. Agregar animal \n 2. Ver todos los animales del mini-zoo \n 3. Salir \n"))
+    if opcion == 1:
+        pass
+    elif opcion == 2:
+        pass
+    elif opcion == 3:
+        break
