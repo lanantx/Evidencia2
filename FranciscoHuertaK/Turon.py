c740ec4b4b138f04e24c4fc607d6e2e492cbee57
Francisco Huerta Kretsch <francisco.huertak@uanl.edu.mx>
2024-04-25 10:07:05 -0600

    Se añaden metodos al Turon

--- FranciscoHuertaK/Turon.py
+++ FranciscoHuertaK/Turon.py
@@ -5,3 +5,18 @@ class Turon:
         self.__sexo = sexo
         self.__largo = largo
         self.__peso = peso
+
+    def alimentacion(self):
+        print("Los Turones comen carne.")
+
+    def caracteristicas(self):
+        print("Son mas robustos y compactos que otros miembros del genero Mustela.")
+
+    def conservacion(self):
+        print("No estan en peligro.")
+
+    def distribucion(self):
+        print("Se hallan en Europa.")
+
+    def nombreCientifico(self):
+        print("Mustela putorius")
