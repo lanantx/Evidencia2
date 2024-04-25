3381b7545b7d7fb445e6ad1af2dc9ea7e8671476
Diego Emilio Ramirez Garcia <diego.emilio.raga@gmail.com>
2024-04-25 15:17:55 -0600

    getters y setter de Tigre

--- DiegooRamirez/Tigre.py
+++ DiegooRamirez/Tigre.py
@@ -4,3 +4,31 @@ class Tigre:
         self.__Edad=Edad
         self.__Peso=Peso
         self.__Tamaño=Tamaño
+
+    @property
+    def nombre(self):
+        return self.__Nombre
+    @nombre.setter
+    def nombre(self,valor):
+        self.__Nombre=valor
+    
+    @property
+    def edad(self):
+        return self.__Edad
+    @edad.setter
+    def edad(self, valor):
+        self.__Edad=valor
+
+    @property
+    def peso(self):
+        return self.__Peso
+    @peso.setter
+    def peso(self,valor):
+        self.__Peso=valor
+
+    @property
+    def tamaño(self):
+        return self.__Tamaño
+    @tamaño.setter
+    def tamaño(self,valor):
+        self.__Tamaño=valor
