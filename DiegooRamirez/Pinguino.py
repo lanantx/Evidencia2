155660f3e61d24cc5b4e04830c3dd0b6d21a8007
Diego Emilio Ramirez Garcia <diego.emilio.raga@gmail.com>
2024-04-25 14:38:22 -0600

    init de Pinguino

---
+++ DiegooRamirez/Pinguino.py
@@ -0,0 +1,8 @@
+class Pinguino:
+    def __init__(self, Nombre, Edad, Peso, Tamaño, Vuela_novuela) -> None:
+        self.__Nombre=Nombre
+        self.__Edad=Edad
+        self.__Peso=Peso
+        self.__Tamaño=Tamaño
+        self.__Vuela_novuela=Vuela_novuela
+        
