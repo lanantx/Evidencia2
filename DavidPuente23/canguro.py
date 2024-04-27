4548c3d0016c6a6623585598c6e991c5f5850e88
David <david14.dapmi@gmail.com>
2024-04-27 09:47:58 -0600

    clase y funciones asignadas

---
+++ DavidPuente23/canguro.py
@@ -0,0 +1,13 @@
+class canguro:
+    def __init__(self,nombre,edad,peso):
+        self.nombre = nombre
+        self.edad = edad
+        self.peso = peso
+
+    def sonido(self):
+        print("BOING BOING")
+
+    def socializar(self):
+        print(f"{self.nombre}, puede que sea muy sociable ya que siempre anda en grupo")
+
+    
