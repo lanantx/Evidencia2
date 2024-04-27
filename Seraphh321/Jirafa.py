cd3d3bd941d2bcfa50cdda9daf66df0d7255ec55
Seraphh321 <167493006+Seraphh321@users.noreply.github.com>
2024-04-27 17:25:05 -0600

    metodos en jirafa

--- Seraphh321/Jirafa.py
+++ Seraphh321/Jirafa.py
@@ -5,5 +5,20 @@ class jirafa:
         self.peso = peso
         self.sexo = sexo
         self.tamaño = tamaño
+        
+    def comida(self):
+            print(f"{self.__nombre}, come hiervas ademas de comer increiblemente 34kg de estas mismas!")
+      
+    def cuello(self):
+            print(f"{self.__nombre} su cuello es excepcionalmente grande alcanzando los soprendentes 3 metros y medio")
+
+    def horario(self):
+            print(f"{self.__nombre} se mantiene activ@ durante el dia y descansa por las noches")
+
+    def habitat(self):
+            print(f"{self.__nombre} esta comod@ en su habitat y no parece molestarle algo")
             
+    def comportamiento(self): 
+            print("su comportamiento es sereno y generalmente solo come")
+           
      
