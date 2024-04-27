33fd6e8caa45017060d214ba26eb90900b4d83db
Daniel Arreaga <d.arreaga236@gmail.com>
2024-04-26 20:58:11 -0600

    getters y setters class Cebra

--- dannyarreaga236/cebra.py
+++ dannyarreaga236/cebra.py
@@ -7,3 +7,59 @@ class Cebra:
         self.__longitud = longitud
         self.__especie = especie
         self.__pareja = pareja
+    
+    @property
+    def nombre(self):
+        return self.__nombre
+    
+    @nombre.setter
+    def nombre(self, value):
+        self.__nombre = value
+    
+    @property
+    def edad(self):
+        return self.__edad
+    
+    @edad.setter
+    def edad(self, value):
+        self.__edad = value
+    
+    @property
+    def sexo(self):
+        return self.__sexo
+    
+    @sexo.setter
+    def sexo(self, value):
+        self.__sexo = value
+    
+    @property
+    def peso(self):
+        return self.__peso
+    
+    @peso.setter
+    def peso(self, value):
+        self.__peso = value
+    
+    @property
+    def longitud(self):
+        return self.__longitud
+    
+    @longitud.setter
+    def longitud(self, value):
+        self.__longitud = value
+    
+    @property
+    def especie(self):
+        return self.__especie
+    
+    @especie.setter
+    def especie(self, value):
+        self.__especie = value
+    
+    @property
+    def pareja(self):
+        return self.__pareja
+    
+    @pareja.setter
+    def pareja(self, value):
+        self.__pareja = value
