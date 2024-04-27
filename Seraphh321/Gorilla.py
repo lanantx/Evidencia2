a4e1f427ce14af032e3b90db9d42368d14d9420b
Seraphh321 <167493006+Seraphh321@users.noreply.github.com>
2024-04-27 16:34:41 -0600

    Metodos en Gorilla
    Los metodos del gorilla fueron agregados.

--- Seraphh321/Gorilla.py
+++ Seraphh321/Gorilla.py
@@ -1,7 +1,22 @@
 class gorilla:
-      def __init__(self, nombre, edad, peso, sexo, tamaño)
+      def __init__(self, nombre, edad, peso, sexo, tamaño):
           self.nombre = nombre
           self.edad = edad
           self.peso = peso
           self.sexo = sexo
           self.tamaño = tamaño
+            
+      def comida(self):
+            print(f"{self.__nombre}, puede comer hasta 30kg de plantas ya que es muy grande}")
+      
+      def horario(self):
+            print(f"{self.__nombre} se mantiene despierto y activo durante las mañanas y empieza a descansar en los atardeceres")
+
+      def comportamiento(self):
+            print(f"{self.__nombre} no es agresivo, usalmente se mantiene sereno pero se llega a estresar cuando hay demasiada gente hablando")
+
+      def masa_muscular(self)
+            print(f"{self.__nombre} esta lleno de musculos por lo tanto tiene demasiada fuerza")
+            
+      def recinto(self) 
+            print("al tener tanta fuerza, su recinto es un poco... especial")
