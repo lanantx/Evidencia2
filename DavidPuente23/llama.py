cb1428b9032908183fea1c0ffd09946e765382d8
David <david14.dapmi@gmail.com>
2024-04-27 10:04:08 -0600

    clase y funciones han sido agregadas

---
+++ DavidPuente23/llama.py
@@ -0,0 +1,13 @@
+class llama:
+    def __init__(self,nombre,edad,peso):
+        self.nombre = nombre
+        self.edad = edad
+        self.peso = peso
+
+    def sonido(self):
+        print("TE ESCUPE")
+
+    def intelectual(self):
+        print(f"{self.nombre}, es demasiado inteligente al igual que todos los de su especie")
+
+    
