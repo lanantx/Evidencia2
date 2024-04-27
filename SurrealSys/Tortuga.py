aee3b577e34cebb4a580b1cc4130823ed1800eef
Daniel Salazar <sergio.salazarsnch@uanl.edu.mx>
2024-04-26 18:35:41 -0600

    Commit final

--- SurrealSys/Tortuga.py
+++ SurrealSys/Tortuga.py
@@ -1,2 +1,73 @@
 class Tortuga:
-    pass
+    def __init__(self, nombre, edad, sexo, peso, sonido):
+        self._nombre = nombre
+        self._edad = edad
+        self._sexo = sexo
+        self._peso = peso
+        self._sonido = sonido
+
+    # atributos: comer, Dormir, hacer_sonido, defecar
+
+    def comer(self,comida):
+        return f"{self._nombre} está comiendo {comida}."
+
+    def dormir(self):
+        return f"{self._nombre} está durmiendo zZzZz."
+
+    def hacer_sonido(self):
+            return f"{self._nombre} dice: {self._sonido}"
+    
+    def reproducirse(self):
+        return f"{self._nombre} esta reproduciendose con el sexo opuesto"
+    
+    def defecar(self): 
+        return f"{self._nombre} esta defecando" 
+
+## Getters & Setters (nombre,edad,sexo,peso,sonido)
+
+    # nombre
+    def get_nombre(self):
+        return self._nombre
+
+    def set_nombre(self, nuevo_nombre):
+        self._nombre = nuevo_nombre
+
+    # edad
+    def get_edad(self):
+        return self._edad
+
+    def set_edad(self, nueva_edad):
+        if nueva_edad >= 0:
+            self._edad = nueva_edad
+        else:
+            print("La edad no puede ser un número negativo.")
+
+    # sexo
+    def get_sexo(self):
+        return self._sexo
+
+    def set_sexo(self, nuevo_sexo):
+        self._sexo = nuevo_sexo
+
+    # peso
+    def get_peso(self):
+        return self._peso
+
+    def set_peso(self, nuevo_peso):
+        if nuevo_peso >= 0:
+            self._peso = nuevo_peso
+        else:
+            print("El peso no puede ser un número negativo.")
+
+    # sonido
+    def get_sonido(self):
+        return self._sonido
+
+    def set_sonido(self, nuevo_sonido):
+        self._sonido = nuevo_sonido
+
+
+    def __str__(self):
+        return f"Nombre: {self._nombre}, Edad: {self._edad}, Sexo: {self._sexo}, Peso: {self._peso}kg, Sonido: {self._sonido}"
+
+
--- SurrealSys/main.py
+++ SurrealSys/main.py
@@ -21,7 +21,6 @@ while True:
         op2 = int(input("Elige un animal que quieras añadir: 1) Cerdo 2) Conejo 3) Tortuga: "))
         if op2 == 1:
             animal = "Cerdo"
-        ##nombre, edad, sexo, peso, 
             nombre = input("Ingresale un nombre: ")
             edad = int(input("Ingresa la edad: "))
             sexo = input("Ingresa el sexo: ")
@@ -34,18 +33,39 @@ while True:
             print(f"Se ha agregado el {animal}")
         elif op2 == 2:
             animal = "Conejo"
+            nombre = input("Ingresale un nombre: ")
+            edad = int(input("Ingresa la edad: "))
+            sexo = input("Ingresa el sexo: ")
+            peso = int(input("Ingresa su peso: "))
             animales.append(animal)
+            animales.append(nombre)
+            animales.append(edad)
+            animales.append(sexo)
+            animales.append(peso)
             print(f"Se ha agregado el {animal}")
         elif op2 == 3:
             animal = "Tortuga"
+            nombre = input("Ingresale un nombre: ")
+            edad = int(input("Ingresa la edad: "))
+            sexo = input("Ingresa el sexo: ")
+            peso = int(input("Ingresa su peso: "))
             animales.append(animal)
+            animales.append(nombre)
+            animales.append(edad)
+            animales.append(sexo)
+            animales.append(peso)
             print(f"Se ha agregado el {animal}")
         else:
             print("Tipo de animal no válido.")
     elif op == 2:
         print("\nAnimales:")
-        for animal in animales:
-            print(f"{animales}")        
+        for i in range(0, len(animales), 5):
+            print("Nombre:", animales[i])
+            print("Edad:", animales[i + 1])
+            print("Sexo:", animales[i + 2])
+            print("Peso:", animales[i + 3])
+            print()
+
     elif op == 3:
         print("Saliendo del programa :D") 
         break
