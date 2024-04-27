c3695308eb6bc774c8e0985f3502ea5c3887dae1
Daniel Arreaga <d.arreaga236@gmail.com>
2024-04-26 20:30:12 -0600

    init class Cebra

--- dannyarreaga236/cebra.py
+++ dannyarreaga236/cebra.py
@@ -1,2 +1,9 @@
 class Cebra:
-    pass
+    def __init__(self, nombre, edad, sexo, peso, longitud, especie, pareja):
+        self.__nombre = nombre
+        self.__edad = edad
+        self.__sexo = sexo
+        self.__peso = peso
+        self.__longitud = longitud
+        self.__especie = especie
+        self.__pareja = pareja
