90682be98997d6eefbc456586e421f2ae29bd2b1
Vanessa Nohely Arrambide Escamilla <arrambidenohely2003@gmail.com>
2024-04-26 23:12:48 -0600

    getters y setters_ coyote
    getters y setters_ coyote

--- VanessaNohely/Coyote.py
+++ VanessaNohely/Coyote.py
@@ -5,3 +5,43 @@ class Coyote:
         self.__edad = edad
         self.__peso = peso
         self.__altura = altura
+        
+        @property 
+        def nombre(self):
+            return self.__nombre
+        
+        @nombre.setter
+        def nombre(self, value):
+            self.__nombre = value
+        
+        @property 
+        def sexo(self):
+            return self.__sexo
+        
+        @sexo.setter
+        def sexo(self, value):
+            self.__sexo = value
+        
+        @property 
+        def edad(self):
+            return self.__edad
+        
+        @edad.setter
+        def edad(self, value):
+            self.__edad = value
+
+        @property 
+        def peso(self):
+            return self.__peso
+        
+        @peso.setter
+        def peso(self, value):
+            self.__peso = value
+        
+        @property 
+        def altura(self):
+            return self.__altura
+        
+        @altura.setter
+        def altura(self, value):
+            self.__altura = value
