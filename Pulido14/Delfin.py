cdb26101b50cb26793b9d19250d8afd40089188b
Pulido14 <tobytobiaswillson@gmail.com>
2024-04-27 08:25:41 -0600

    creando init

--- Pulido14/Delfin.py
+++ Pulido14/Delfin.py
@@ -1,1 +1,8 @@
 class Delfin:
+    def __init__(self, nombre, edad, longitud, peso, especie):
+        self._nombre = nombre
+        self._edad = edad
+        self._longitud = longitud
+        self._peso = peso
+        self._especie = especie
+   
