ab25b855161593b9f1f751f8e381a6ea2417062e
Vanessa Nohely Arrambide Escamilla <arrambidenohely2003@gmail.com>
2024-04-26 22:56:28 -0600

    commit __init__ pinguino
    commit __init__ pinguino

---
+++ VanessaNohely/pinguino.py
@@ -0,0 +1,8 @@
+class Pinguino:
+    def __init__(self, nombre, sexo, edad, peso, altura):
+        self.__nombre = nombre
+        self.__sexo = sexo 
+        self.__edad = edad
+        self.__peso = peso
+        self.__altura = altura
+        
