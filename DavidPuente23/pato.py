6c3bc578bb1dc897d8cec50b7ddaf099416d707d
David <david14.dapmi@gmail.com>
2024-04-27 09:36:13 -0600

    getter y setter asignados

--- DavidPuente23/pato.py
+++ DavidPuente23/pato.py
@@ -10,5 +10,28 @@ class pato:
     def vida (self):
         print(f"{self.nombre}, puede vivir hasta 10 años")
 
-        
+    @property
+    def nombre(self):
+        return self.__nombre
+    @nombre.setter
+    def nombre(self,valor):
+        self.__nombre=valor
+
+    @property
+    def edad(self):
+        return self.__edad
+    @edad.setter
+    def edad(self,valor):
+        self.__edad=valor
+
+    @property
+    def peso(self):
+        return self.__peso
+    @peso.setter
+    def peso(self,valor):
+        self.__peso=valor
+
+    
+
+
 
