da4feafb26359e46a0a7da79721a9cb8e0c571d7
Vanessa Nohely Arrambide Escamilla <arrambidenohely2003@gmail.com>
2024-04-26 23:02:16 -0600

    commit __init__ burro
    commit __init__ burro

---
+++ VanessaNohely/burro.py
@@ -0,0 +1,7 @@
+class Burro:
+    def __init__(self, nombre, sexo, edad, peso, altura):
+        self.__nombre = nombre
+        self.__sexo = sexo
+        self.__edad = edad
+        self.__peso = peso
+        self.__altura = altura
