e271208f7e98c193fc45605797869a26c26bede2
Seraphh321 <167493006+Seraphh321@users.noreply.github.com>
2024-04-28 16:38:49 -0600

    Borre cosas innecesarias de jirafa

--- Seraphh321/Jirafa.py
+++ Seraphh321/Jirafa.py
@@ -7,16 +7,16 @@ class jirafa:
         self.tamaño = tamaño
         
     def comida(self):
-            print(f"{self.__nombre}, come hiervas ademas de comer increiblemente 34kg de estas mismas!")
+            print( "come hiervas ademas de comer increiblemente 34kg de estas mismas!")
       
     def cuello(self):
-            print(f"{self.__nombre} su cuello es excepcionalmente grande alcanzando los soprendentes 3 metros y medio")
+            print( "su cuello es excepcionalmente grande alcanzando los soprendentes 3 metros y medio")
 
     def horario(self):
-            print(f"{self.__nombre} se mantiene activ@ durante el dia y descansa por las noches")
+            print( "se mantiene activ@ durante el dia y descansa por las noches")
 
     def habitat(self):
-            print(f"{self.__nombre} esta comod@ en su habitat y no parece molestarle algo")
+            print(" esta comod@ en su habitat y no parece molestarle algo")
             
     def comportamiento(self): 
             print("su comportamiento es sereno y generalmente solo come")
@@ -57,7 +57,4 @@ class jirafa:
     def tamaño(self,valor):
             self.__tamaño=valor
 
-
-    def __str__(self):
-    return f"Jirafa:  1 Nombre: {self.__nombre} 2 Edad: {self.__edad} 3 Peso: {self.__peso} 4 Tamaño: {self.__tamaño} 5 Sexo: {self.__sexo}"
 
