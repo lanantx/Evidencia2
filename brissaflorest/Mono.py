1ae0c087ee0d6289c909632b3d4f44e12aa30036
Brissa Marisol Flores Tamez <brissa.florest@uanl.edu.mx>
2024-04-28 15:21:19 -0600

    GETTERS Y SETTERS_Mono

--- brissaflorest/Mono.py
+++ brissaflorest/Mono.py
@@ -6,3 +6,42 @@ class Mono:
         self.__peso = peso
         self.__altura = altura
 
+    @property
+    def nombre(self):
+        return self.__nombre
+    
+    @nombre.setter
+    def nombre(self,valor):
+        self.__nombre = valor   
+    
+    @property
+    def edad(self):
+        return self.__edad
+    
+    @edad.setter
+    def edad(self,valor):
+        self.__edad = valor   
+
+    @property
+    def tamaño(self):
+        return self.__tamaño
+    
+    @tamaño.setter
+    def tamaño(self,valor):
+        self.__tamaño = valor   
+
+    @property
+    def peso(self):
+        return self.__peso
+    
+    @peso.setter
+    def peso(self,valor):
+        self.__peso = valor
+
+    @property
+    def altura(self):
+        return self.__altura
+    
+    @altura.setter
+    def altura(self,valor):
+        self.__altura = valor
