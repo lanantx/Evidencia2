3a76745f76029edb8b4597cbde8e042ca086f495
Pulido14 <tobytobiaswillson@gmail.com>
2024-04-28 17:43:12 -0600

    agregando la primera opcion

--- Pulido14/Main.py
+++ Pulido14/Main.py
@@ -9,3 +9,22 @@ while True:
                       "1. Agregar animal (Ballena, Delfín o Serpiente) \n"
                       "2. Ver todos los animales del mini-zoo \n"
                       "3. Salir \n"))
+  
+  if opcion == 1:
+     tipo_animal = input("¿Qué animal quieres agregar? (Ballena, Delfín o Serpiente): ")
+     nombre = input("¿Cuál es su nombre? \n")
+     edad = input("¿Cuál es su edad? \n")
+     sexo = input("¿Cuál es su sexo? \n")
+     tamaño = input("¿Cuál es su tamaño? \n")
+     peso = input("¿Cuál es su peso? \n")
+
+     if tipo_animal == "Ballena":
+      nuevo_animal = Ballena(nombre, edad, sexo, tamaño, peso)
+     elif tipo_animal == "Delfín":
+      nuevo_animal = Delfin(nombre, edad, sexo, tamaño, peso)
+     elif tipo_animal == "Serpiente":
+      nuevo_animal = Serpiente(nombre, edad, sexo, tamaño, peso)
+     else:
+      print("Animal no reconocido. Favor de elegir entre Ballena, Delfín o Serpiente.")
+
+     animales.append(nuevo_animal)
