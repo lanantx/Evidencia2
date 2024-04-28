968da262dd7811dfaa01ef09dde4e3723066ff4f
Brissa Marisol Flores Tamez <brissa.florest@uanl.edu.mx>
2024-04-28 15:10:26 -0600

    INIT_Koala

--- brissaflorest/Koala.py
+++ brissaflorest/Koala.py
@@ -1,38 +1,8 @@
-class koala:
-    def __init__(self,nombre,edad,tamaño,peso):
+class Koala:
+    def __init__(self,nombre,edad,tamaño,peso,altura):
         self.__nombre = nombre
         self.__edad = edad
         self.__tamaño = tamaño
         self.__peso = peso
+        self.__altura = altura
 
-    @property
-    def nombre(self):
-        return self.__nombre
-    
-    @nombre.setter
-    def nombre(self,valor):
-        self.__nombre = valor   
-    
-    @property
-    def edad(self):
-        return self.__edad
-    
-    @edad.setter
-    def edad(self,valor):
-        self.__edad = valor   
-
-    @property
-    def tamaño(self):
-        return self.__tamaño
-    
-    @tamaño.setter
-    def tamaño(self,valor):
-        self.__tamaño = valor   
-
-    @property
-    def peso(self):
-        return self.__peso
-    
-    @peso.setter
-    def peso(self,valor):
-        self.__peso = valor
