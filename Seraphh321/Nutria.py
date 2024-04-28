294eb4667bcc92d3e5534ab77977d115137ca7b7
Seraphh321 <167493006+Seraphh321@users.noreply.github.com>
2024-04-27 18:14:15 -0600

    init nutria

--- Seraphh321/Nutria.py
+++ Seraphh321/Nutria.py
@@ -1,2 +1,9 @@
 class Nutria:
-    pass
+      def __init__(self, nombre, edad, peso, sexo, tamaño):
+          self.nombre = nombre
+          self.edad = edad
+          self.peso = peso
+          self.sexo = sexo
+          self.tamaño = tamaño
+            
+     
