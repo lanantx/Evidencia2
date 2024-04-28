8a439715a45efd9339bada8ae11a934ad9693889
Brissa Marisol Flores Tamez <brissa.florest@uanl.edu.mx>
2024-04-28 16:26:20 -0600

    METODOS_Mono

--- brissaflorest/Mono.py
+++ brissaflorest/Mono.py
@@ -45,3 +45,21 @@ class Mono:
     @altura.setter
     def altura(self,valor):
         self.__altura = valor
+
+    def __str__(self):
+        return f"Mono | Nombre: {self.__nombre} | Edad: {self.__edad} | Tamaño: {self.__tamaño} | Peso: {self.__peso} | Altura: {self.__altura}."
+
+    def comer(self):
+        print("Los monos son omnívoros, es decir, que van a comer todo aquello que encuentren.")
+
+    def aspecto(self):
+        print("Los monos tienen un gran parecido a los humanos, tanto en forma de ser como en lo físico.")
+    
+    def trepar(self):
+        print("Los monos cuentan con colas prensiles que utilizan para facilitar la atarea de trepar los árboles.")
+
+    def jugar(self):
+        print("Los monos lo consideran muy importante en sus vidas. Jugar les divierte y les hace disfrutar.")
+
+    def dormir(self):
+        print("Los monos duermen aproximadamente 9 o 10 horas y sus ciclos de sueño son de 80 minutos.")
