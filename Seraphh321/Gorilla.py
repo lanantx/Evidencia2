adeca1c256b0fe60a35ca5ceaddfe3294f897334
Seraphh321 <gibranaleman345@gmail.com>
2024-04-28 17:23:09 -0600

    metodos gorilla

--- Seraphh321/Gorilla.py
+++ Seraphh321/Gorilla.py
@@ -5,6 +5,21 @@ class gorilla:
           self.peso = peso
           self.sexo = sexo
           self.tamaño = tamaño
+      def comida(self):
+            print(" puede comer hasta 30kg de plantas ya que es muy grande")
+      
+      def horario(self):
+            print(" se mantiene despierto y activo durante las mañanas y empieza a descansar en los atardeceres")
+
+      def comportamiento(self):
+            print(" no es agresivo, usalmente se mantiene sereno pero se llega a estresar cuando hay demasiada gente hablando")
+
+      def masa_muscular(self):
+            print(" esta lleno de musculos por lo tanto tiene demasiada fuerza")
+            
+      def recinto(self): 
+            print("al tener tanta fuerza, su recinto es un poco... especial")
+    
 
             
       
