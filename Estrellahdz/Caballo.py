1af98dabaae701650e429508038b5a72607c4654
Estrellahdz <estrella.05hdz@gmail.com>
2024-04-28 16:01:47 -0600

    actualizar clase caballo

--- Estrellahdz/Caballo.py
+++ Estrellahdz/Caballo.py
@@ -5,4 +5,47 @@ class Caballo:
         self.__edad = edad
         self.__peso = peso
         self.__altura = altura
+
+        @property
+        def nombre(self):
+            return self.__nombre
         
+        @nombre.setter
+        def nombre(self, value):
+            self.__nombre = value 
+
+        @property 
+        def sexo(self):
+            return self.__sexo
+        
+        @sexo.setter
+        def sexo(self, value):
+            self.__sexo = value
+
+        @property 
+        def edad(self):
+            return self.__edad
+        
+        @edad.setter
+        def edad(self, value):
+            self.__edad = value 
+
+        @property
+        def peso(self):
+            return self.__peso
+        
+        @peso.setter 
+        def peso(self, value):
+            self.__peso = value 
+
+        @property 
+        def altura(self):
+            return self.__altura
+        
+        @altura.setter
+        def altura(self, value):
+            self.__altura = altura
+
+
+    def __str__(self):
+        return (f"Caballo: \n Nombre: {self.__nombre}\n Sexo: {self.__sexo}\n Edad: {self.__edad}\n Peso: {self.__peso}\n Altura: {self.__altura}")
