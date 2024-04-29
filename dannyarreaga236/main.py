a2682930da79519e9d7dd06dbbea7b5a120f24a1
Daniel Arreaga <d.arreaga236@gmail.com>
2024-04-28 23:16:10 -0600

    Menu realizado en archivo main con match

--- dannyarreaga236/main.py
+++ dannyarreaga236/main.py
@@ -1,1 +1,17 @@
 #Programa principal
+import lemur
+import oso
+import cebra
+
+while True:
+    print(f'MENU\n1. Agregar animal\n2. Ver todos los animales del mini-zoo\n3. Salir')
+    opc = int(input('Elige una opcion: '))
+    match(opc):
+        case 1:
+            pass
+        case 2:
+            pass
+        case 3:
+            break
+        case _:
+            print("ERROR. Ingrese una opcion valida.")
