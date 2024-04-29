a151e6ad35e2ed3a414028e67b8362f2fbf5efc6
Daniel Arreaga <d.arreaga236@gmail.com>
2024-04-28 23:34:38 -0600

    Opcion1 finalizada

--- dannyarreaga236/main.py
+++ dannyarreaga236/main.py
@@ -3,12 +3,46 @@ import lemur
 import oso
 import cebra
 
+cebras = []
+osos = []
+lemures = []
 while True:
     print(f'MENU\n1. Agregar animal\n2. Ver todos los animales del mini-zoo\n3. Salir')
     opc = int(input('Elige una opcion: '))
     match(opc):
         case 1:
-            pass
+            print("\n1. Cebra\n2. Oso\n3. Lemur")
+            animal = int(input("\nQue animal agregara? "))
+            if animal == 1:
+                nombre = input("Nombre: ")
+                edad = input("Edad: ")
+                sexo = input("Sexo: ")
+                peso = input("Peso: ")
+                longitud = input("Longitud: ")
+                especie = input("Especie: ")
+                pareja = input("Pareja: ")
+                nuevaCebra = cebra.Cebra(nombre, edad, sexo, peso, longitud, especie, pareja)
+                cebras.append(nuevaCebra)
+            elif animal == 2:
+                nombre = input("Nombre: ")
+                edad = input("Edad: ")
+                sexo = input("Sexo: ")
+                peso = input("Peso: ")
+                altura = input("Altura: ")
+                pareja = input("Pareja: ")
+                nuevoOso = oso.Oso(nombre, edad, sexo, peso, altura, pareja)
+                osos.append(nuevoOso)
+            elif animal == 3:
+                nombre = input("Nombre: ")
+                edad = input("Edad: ")
+                sexo = input("Sexo: ")
+                peso = input("Peso: ")
+                largo = input("Largo: ")
+                pareja = input("Pareja: ")
+                nuevoLemur = lemur.Lemur(nombre, edad, sexo, peso, largo, pareja)
+                lemures.append(nuevoLemur)
+            else:
+                print("Ingrese una opcion valida")
         case 2:
             pass
         case 3:
