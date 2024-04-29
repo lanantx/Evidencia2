8d256106dce733126ed4277b4eec14b33da93ded
Daniel Arreaga <d.arreaga236@gmail.com>
2024-04-28 21:38:50 -0600

    getters y setters de class oso

--- dannyarreaga236/oso.py
+++ dannyarreaga236/oso.py
@@ -4,5 +4,55 @@ class Oso:
         self.__edad = edad
         self.__sexo = sexo
         self.__peso = peso
-        self.__largo = altura
+        self.__altura = altura
         self.__pareja = pareja
+    
+    @property
+    def nombre(self):
+        return self.__nombre
+    
+    @nombre.setter
+    def nombre(self, value):
+        self.__nombre = value
+    
+    @property
+    def edad(self):
+        return self.__edad
+    
+    @edad.setter
+    def edad(self, value):
+        if value <0:
+            print("La edad tiene que ser un numero positivo")
+        self.__edad = value
+    
+    @property
+    def sexo(self):
+        return self.__sexo
+    
+    @sexo.setter
+    def sexo(self, value):
+        self.__sexo = value
+    
+    @property
+    def peso(self):
+        return self.__peso
+    
+    @peso.setter
+    def peso(self, value):
+        self.__peso = value
+    
+    @property
+    def altura(self):
+        return self.__altura
+    
+    @altura.setter
+    def altura(self, value):
+        self.__altura = value
+    
+    @property
+    def pareja(self):
+        return self.__pareja
+    
+    @pareja.setter
+    def pareja(self, value):
+        self.__pareja = value
