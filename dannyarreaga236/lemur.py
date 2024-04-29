94c3b19f8bbaed6d73a98ccbbb399a2a273a2694
Daniel Arreaga <d.arreaga236@gmail.com>
2024-04-28 19:53:52 -0600

    Agrego el init de la clase del animal Lemur

--- dannyarreaga236/lemur.py
+++ dannyarreaga236/lemur.py
@@ -1,2 +1,8 @@
 class Lemur:
-    pass
+     def __init__(self, nombre, edad, sexo, peso, longitud, pareja):
+        self.__nombre = nombre
+        self.__edad = edad
+        self.__sexo = sexo
+        self.__peso = peso
+        self.__longitud = longitud
+        self.__pareja = pareja
