79c0cbf54271518b2fba4c6b4bee08778729f1d5
Daniel Arreaga <d.arreaga236@gmail.com>
2024-04-28 20:24:07 -0600

    Correccion de identacion de metodo init class lemur

--- dannyarreaga236/lemur.py
+++ dannyarreaga236/lemur.py
@@ -1,8 +1,8 @@
 class Lemur:
-     def __init__(self, nombre, edad, sexo, peso, largo, pareja):
-          self.__nombre = nombre
-          self.__edad = edad
-          self.__sexo = sexo
-          self.__peso = peso
-          self.__largo = largo
-          self.__pareja = pareja
+    def __init__(self, nombre, edad, sexo, peso, largo, pareja):
+        self.__nombre = nombre
+        self.__edad = edad
+        self.__sexo = sexo
+        self.__peso = peso
+        self.__largo = largo
+        self.__pareja = pareja
