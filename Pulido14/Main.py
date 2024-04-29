0f9acf8c902c6ba915ee30aa481d1a3c2e768f17
Pulido14 <tobytobiaswillson@gmail.com>
2024-04-28 18:05:50 -0600

    agregando la opcion 2 y 3

--- Pulido14/Main.py
+++ Pulido14/Main.py
@@ -9,22 +9,33 @@ while True:
                       "1. Agregar animal (Ballena, Delfín o Serpiente) \n"
                       "2. Ver todos los animales del mini-zoo \n"
                       "3. Salir \n"))
-  
+
   if opcion == 1:
-     tipo_animal = input("¿Qué animal quieres agregar? (Ballena, Delfín o Serpiente): ")
-     nombre = input("¿Cuál es su nombre? \n")
-     edad = input("¿Cuál es su edad? \n")
-     sexo = input("¿Cuál es su sexo? \n")
-     tamaño = input("¿Cuál es su tamaño? \n")
-     peso = input("¿Cuál es su peso? \n")
+    tipo_animal = input("¿Qué animal quieres agregar? (Ballena, Delfín o Serpiente): ")
+    nombre = input("¿Cuál es su nombre? \n")
+    edad = input("¿Cuál es su edad? \n")
+    sexo = input("¿Cuál es su sexo? \n")
+    tamaño = input("¿Cuál es su tamaño? \n")
+    peso = input("¿Cuál es su peso? \n")
 
-     if tipo_animal == "Ballena":
+    if tipo_animal == "Ballena":
       nuevo_animal = Ballena(nombre, edad, sexo, tamaño, peso)
-     elif tipo_animal == "Delfín":
+    elif tipo_animal == "Delfín":
       nuevo_animal = Delfin(nombre, edad, sexo, tamaño, peso)
-     elif tipo_animal == "Serpiente":
+    elif tipo_animal == "Serpiente":
       nuevo_animal = Serpiente(nombre, edad, sexo, tamaño, peso)
-     else:
+    else:
       print("Animal no reconocido. Favor de elegir entre Ballena, Delfín o Serpiente.")
+      continue
 
-     animales.append(nuevo_animal)
+    animales.append(nuevo_animal)
+
+  elif opcion == 2:
+    for animal in animales:
+      print(animal)
+
+  elif opcion == 3:
+    break
+
+
+  
