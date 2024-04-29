81e08bb981e179b70ed2135bd7f670ba88accb54
Pulido14 <tobytobiaswillson@gmail.com>
2024-04-28 18:52:13 -0600

    creando init Serpiente

---
+++ Pulido14/Ballena.py
@@ -0,0 +1,7 @@
+class Ballena:
+   def __init__(self, nombre, edad, longitud, peso, especie):
+        self._nombre = nombre
+        self._edad = edad
+        self._longitud = longitud
+        self._peso = peso
+        self._especie = especie
